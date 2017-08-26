6b46cc8cb17c90e5b1434660690aede651565909
timcoda <timcoda2132@gmail.com>
2017-08-26 12:09:57 +0800

    passed debugger

--- src/regular_expression/Q10058_2.java
+++ src/regular_expression/Q10058_2.java
@@ -1,5 +1,5 @@
 /*
- * https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=999
+ * https://uva.onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=999
  * 10058 - Jimmi's Riddles
  */
 package regular_expression;
@@ -40,41 +40,135 @@ public class Q10058_2 {
 	private static final String NOUN = "tom|jerry|goofy|mickey|jimmy|dog|cat|mouse";
 	private static final String ARTICLE = "a|the";
 	private static final String SPACE = "\\s+";
+	private static final String CORRECT = "YES I WILL";
+	private static final String INCORRECT = "NO I WON'T";
+	private static final String CONJUNCTION = " and ";
+	private static final Pattern LAST_CONJUNCTION = Pattern.compile("\\sand\\s\\w+\\Z");
+	private static final Pattern PATTERN_VERB = Pattern.compile(VERB);
+	private static final String STATEMENT_CONJUCNTION = "\\s?,\\s?";
+	private static final Pattern PATTERN_STATEMENT_CONJUNCTION = Pattern.compile(STATEMENT_CONJUCNTION);
+	private static final Pattern LAST_STATEMENT_CONJUNCTION = Pattern.compile("\\s?,\\s?[\\w\\s]+\\Z");
+			
 	
-//	private static final Pattern PATTERN_VREB = Pattern.compile(VERB);
-//	private static final Pattern PATTERN_NOUN = Pattern.compile(NOUN);
-//	private static final Pattern PATTERN_ARTICLE = Pattern.compile(ARTICLE);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Q10058_2 solver = new Q10058_2();
+		String input = null;
+		while((input = Q10058_2.ReadLn(MAX)) != null){
+			solver.getAnswer(input);
+		}
+		return;
+	}
 	
-//	private Matcher matcherVerb = null;
-//	private Matcher matcherNoun = null;
-//	private Matcher matcherArticle = null;
+	public void getAnswer(String input){
+		if(input == null) 
+			return;
+		
+		if(isStatement(input)){
+			System.out.println(CORRECT);
+		}else{
+			System.out.println(INCORRECT);
+		}
+	}
 	
-	private boolean isVerb(String input){
+	public boolean isStatement(String input){
+		input = input.trim();
 		boolean result = false;
 		if(input != null){
-			result = input.trim().matches(VERB);
+			Matcher matcher = LAST_STATEMENT_CONJUNCTION.matcher(input);
+			if(matcher.find()){
+				String frontPart = input.substring(0, matcher.start());
+				String endPart = input.substring(matcher.start());
+				
+//				System.out.println("the end part is: >>>>>>>>>>>>>>>" + endPart);
+				Matcher conjunctionMatcher = PATTERN_STATEMENT_CONJUNCTION.matcher(endPart);
+				if(conjunctionMatcher.find()){
+					endPart = endPart.substring(conjunctionMatcher.end());
+				}
+				 
+//				System.out.println("the front: "+frontPart);
+//				System.out.println("end front: "+endPart);
+				result = isAction(endPart) && isStatement(frontPart);
+			}else{
+				result = isAction(input);
+			}
+		}
+		return result;
+	}
+	
+	public boolean isAction(String input){
+		input = input.trim();
+		boolean result = false;
+		if(input != null){
+			Matcher verbMatcher = PATTERN_VERB.matcher(input);
+			if(verbMatcher.find()){
+				String part1 = input.substring(0, verbMatcher.start());
+				String part2 = input.substring(verbMatcher.end());
+				result = isActiveList(part1) && isActiveList(part2);
+			}else{
+				result = false;
+			}
+		}
+		return result;
+		
+	}
+	
+	
+	public boolean isActiveList(String input){
+		input = input.trim();
+		boolean result = false;
+		if(input != null){
+			if(isActor(input)){
+				return true;
+			}else{
+				Matcher matcher = LAST_CONJUNCTION.matcher(input);
+				if(matcher.find()){
+					String trailingPart = input.substring(matcher.start() + CONJUNCTION.length());
+					if(isActor(trailingPart)){
+						String remaining = input.substring(0, matcher.start());
+						return isActiveList(remaining);
+					}else{
+						return false;
+					}
+				}else{
+					return false;
+				}
+			}
+		}
+		return result;
+	}
+	
+	private boolean isVerb(String input){
+		input = input.trim();
+		boolean result = false;
+		if(input != null){
+			result = input.matches(VERB);
 		}
 		return result;
 	}
 	
 	private boolean isNoun(String input){
+		input = input.trim();
 		boolean result = false;
 		if(input != null){
-			result = input.trim().matches(NOUN);
+			result = input.matches(NOUN);
 		}
 		return result;
 	}
 	
 	private boolean isArticle(String input){
+		input = input.trim();
 		boolean result = false;
 		if(input != null){
-			result = input.trim().matches(ARTICLE);
+			result = input.matches(ARTICLE);
 			
 		}
 		return result;
 	}
 	
 	private boolean isActor(String input){
+		
+		input = input.trim();
 		boolean result = false;
 		if(input != null){
 			String[] words = input.split(SPACE);
@@ -83,24 +177,13 @@ public class Q10058_2 {
 			}else if(words.length > 1){
 				result = isArticle(words[0]) && isNoun(words[1]);
 			}
-			
 		}
 		return result;
 	}
 	
+
+
 	
-	
-
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Q10058_2 solver = new Q10058_2();
-		String input = null;
-		while((input = Q10058_2.ReadLn(MAX)) != null){
-			System.out.println(input);
-		}
-
-	}
 	
 
 }
